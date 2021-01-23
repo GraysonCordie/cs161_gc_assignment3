c02e3c6de908b74d9d3ed5d0c51e34df7a7e1d52
GraysonCordie <77179585+GraysonCordie@users.noreply.github.com>
2021-01-22 16:34:07 -0800

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,31 @@
+package edu.cgcc.cs161.client;
+
+public class Client {
+	
+	private String firstname; 
+	private String lastname;
+	private String street;
+	private String city;
+	private String state;
+	private String country;
+	private int zipcode;
+	private int phone;
+	private String email;
+	private String insurancecarrier;
+	
+	Client(String fname, String lname, String strt, String cit, String stat, String coun, int zip, int phon, String emal, String insur){
+	//info is set upon creation of client object	
+		firstname = fname;
+		lastname = lname;
+		street = strt;
+		city = cit;
+		state = stat;
+		country = coun;
+		zipcode = zip;
+		phone = phon;
+		email = emal;
+		insurancecarrier = insur;
+		
+	}
+
+}
