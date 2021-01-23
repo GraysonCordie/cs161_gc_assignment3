4ef577cbfed84b154f9b54f5174f7e38c0a8dc53
GraysonCordie <77179585+GraysonCordie@users.noreply.github.com>
2021-01-22 16:37:34 -0800

    Add files via upload

---
+++ ServiceNeed.java
@@ -0,0 +1,16 @@
+package edu.cgcc.cs161.service;
+
+public class ServiceNeed {
+	
+	private String ServiceNeedDescription;
+	
+	public void identifyNeed() {
+		//would create a survey question with question number pertaining to proper question
+	}
+	public void makeAppointment() {
+		
+	}
+	public void makeReferral() {
+		
+	}
+}
