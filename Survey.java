3b7549c942b6b712815025a688fa5f785caa788c
GraysonCordie <77179585+GraysonCordie@users.noreply.github.com>
2021-01-22 16:38:07 -0800

    Add files via upload

---
+++ Survey.java
@@ -0,0 +1,25 @@
+package edu.cgcc.cs161.survey;
+
+import java.util.Scanner;
+
+public class Survey {
+	
+	private int surveynumber;
+	private int questionset;
+	private int answerset;
+	
+	Survey(int snum, int queset, int anset){
+		surveynumber = snum;
+		questionset = queset;
+		answerset = anset;
+	}
+
+	public void displayQestion() {
+		//get question and display
+	}
+
+	
+	public void recordAnswer() {
+		//use scanner to record answer
+	}
+}
