ddf12c72fe76a0fe8acd1f58391c2b5d786a3279
GraysonCordie <77179585+GraysonCordie@users.noreply.github.com>
2021-01-23 19:28:04 -0800

    Add files via upload

---
+++ ServiceNeed.java
@@ -0,0 +1,23 @@
+package edu.cgcc.cs161.service;
+
+import edu.cgcc.cs161.referral.*;
+import edu.cgcc.cs161.calendar.*;
+import edu.cgcc.cs161.survey.*;
+
+public class ServiceNeed {
+	
+	private String ServiceNeedDescription;
+	
+	public void identifyNeed() {
+		Survey newServ = new Survey();
+		
+	}
+	public void makeAppointment() {
+		Appointment newAppo = new Appointment();
+		
+	}
+	public void makeReferral() {
+		Referral newRef = new Referral();
+		
+	}
+}
