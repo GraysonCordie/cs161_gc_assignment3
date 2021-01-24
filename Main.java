2b3a21479f95b0fe0e98a7a5de3e657bb42746a7
GraysonCordie <77179585+GraysonCordie@users.noreply.github.com>
2021-01-23 20:06:49 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package edu.cgcc.cs161.main;
+//HEADER
+//Program Name: Assignment Three
+//Author: Grayson Cordie
+//Class: CS161 Winter 2020
+//Date: 01/24/2021
+//Description: Is basic framework of program based on given class diagram.
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Would call other classes to perform function?
+
+	}
+
+}
+
+/* FOOTER
+ * 
+ */
